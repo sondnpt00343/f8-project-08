ace3be84a35e59887095a28d395ffb0dd483e463
Son Dang <sondnpt00343@gmail.com>
2023-09-14 16:37:01 +0700

    Tái sử dụng Header, Footer trong trang

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+function load(selector, path) {
+    const cached = localStorage.getItem(path);
+    if (cached) {
+        document.querySelector(selector).innerHTML = cached;
+    }
+
+    fetch(path)
+        .then((res) => res.text())
+        .then((html) => {
+            if (html !== cached) {
+                document.querySelector(selector).innerHTML = html;
+                localStorage.setItem(path, html);
+            }
+        });
+}
