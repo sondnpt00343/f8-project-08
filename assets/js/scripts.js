ad1954a9846c297ac5a9ee06a3004b54bf275f83
Son Dang <sondnpt00343@gmail.com>
2023-09-30 21:28:45 +0700

    Responsive phần Navbar #1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -132,3 +132,32 @@ function handleActiveMenu() {
         dropdown.onmouseleave = () => init();
     });
 }
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
