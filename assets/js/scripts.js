36e340e4257b7d6f3540c1101c35c1ed3bdd996b
Son Dang <sondnpt00343@gmail.com>
2023-10-02 15:00:51 +0700

    Responsive phần Navbar #4

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -167,3 +167,15 @@ function initJsToggle() {
         };
     });
 }
+
+window.addEventListener("template-loaded", () => {
+    const links = $$(".js-dropdown-list > li > a");
+
+    links.forEach((link) => {
+        link.onclick = () => {
+            if (window.innerWidth > 991) return;
+            const item = link.closest("li");
+            item.classList.toggle("navbar__item--active");
+        };
+    });
+});
