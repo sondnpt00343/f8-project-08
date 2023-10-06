e73da6ba22aa00e3f994980d2c5c8997f8c946ab
Son Dang <sondnpt00343@gmail.com>
2023-10-06 12:52:22 +0700

    Làm form filter sản phẩm #5

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -154,7 +154,9 @@ function initJsToggle() {
         if (!target) {
             document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
         }
-        button.onclick = () => {
+        button.onclick = (e) => {
+            e.preventDefault();
+
             if (!$(target)) {
                 return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
             }
@@ -164,6 +166,14 @@ function initJsToggle() {
                 $(target).classList.toggle("hide", !isHidden);
                 $(target).classList.toggle("show", isHidden);
             });
+        };
+        document.onclick = function (e) {
+            if (!e.target.closest(target)) {
+                const isHidden = $(target).classList.contains("hide");
+                if (!isHidden) {
+                    button.click();
+                }
+            }
         };
     });
 }
